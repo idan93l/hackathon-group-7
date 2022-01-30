48d517e641c791a67463df93ca1ed0868c48ae8c
Idan Lev <idan.93lev@gmail.com>
2022-01-30 19:22:58 +0200

    idan merge

--- server/app.js
+++ server/app.js
@@ -1,39 +1,47 @@
-
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const axios = require("axios");
-require('dotenv').config()
-app.use(express.json())
-app.use(cors())
-app.use(express.urlencoded({extended : true}))
-require("./db/mongoose")
+require("dotenv").config();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+require("./db/mongoose");
+const searchLyrics =  require("./genius")
 
-app.get('/', (req,res) => {
-    console.log('hello from frontend!')
-})
+searchLyrics("hurt");
 
-
-
-
-app.post('/translate', (req,res) => {
-    const options = {
-        method: 'POST',
-        url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
-        params: {to: 'he', 'api-version': '3.0', profanityAction: 'NoAction', textType: 'plain'},
-        headers: {
-          'content-type': 'application/json',
-          'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
-          'x-rapidapi-key': `${process.env.TRANS_API_KEY}`
-        },
-        data: [{Text: 'I would really like to drive your car around the block a few times.'}]
-    };
-    axios.request(options).then(function (response) {
-        console.log(response.data[0].translations);
-    }).catch(function (error) {
-        console.error(error);
+app.post("/translate", (req, res) => {
+  const options = {
+    method: "POST",
+    url: "https://microsoft-translator-text.p.rapidapi.com/translate",
+    params: {
+      to: "he",
+      "api-version": "3.0",
+      profanityAction: "NoAction",
+      textType: "plain",
+    },
+    headers: {
+      "content-type": "application/json",
+      "x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
+      "x-rapidapi-key": `${process.env.TRANS_API_KEY}`,
+    },
+    data: [
+      {
+        Text: "I would really like to drive your car around the block a few times.",
+      },
+    ],
+  };
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data[0].translations);
+    })
+    .catch(function (error) {
+      console.error(error);
     });
-})
-  
+});
 
-app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`)
+);
