21380ce000492c210b3e6509751487b0d0d49567
tamarcy <cohen.tamar@gmail.com>
2022-01-30 15:22:25 +0200

    Languege-select component

--- client/src/components/LanguageSelect.jsx
+++ client/src/components/LanguageSelect.jsx
@@ -1,12 +1,12 @@
-import react from react
+import React from react
 
-export default function LanguageSelect({languagesArray}) {
+export default function LanguageSelect({languagesArray, handleSelect}) {
     const renderLangues = (languagesArray) => {
-       return  languagesArray.map((language)=> {return <option key={language} value={language}>{language}</option>  )   
+       return  languagesArray.map((language)=> {return <option key={language} value={language}>{language}</option> } )   
     }
     return (
-        <select name="Language" id="Language">
-           {renderLangues()}
+        <select name="Language" id="Language" onChange={handleSelect}>
+           {renderLangues(languagesArray)}
         </select>
     )
 }
