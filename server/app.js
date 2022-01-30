cfe4147b0e50e24a2745e8e386b8a6a0a3fef44a
tamarcy <cohen.tamar@gmail.com>
2022-01-30 18:03:32 +0200

    packges installed

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require("path")
 require('dotenv').config()
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended : true}))
 require("./db/mongoose")
+
 
 app.get('/', (req,res) => {
     console.log('hello from frontend!')
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 
 mongo_uri = process.env.MONGO_URI
+console.log(mongo_uri);
 
 mongoose.connect(mongo_uri)
   .then(() => console.log("Database connected"))
