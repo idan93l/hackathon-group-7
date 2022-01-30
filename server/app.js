b76cba02f8b1670296d5590a322c23b0f42a4371
itai rozen <itai.rozen@mail.huji.ac.il>
2022-01-30 17:02:40 +0200

    translation call is in app.post now

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const axios = require("axios");
 require('dotenv').config()
 app.use(express.json())
 app.use(cors())
@@ -11,23 +12,27 @@ app.get('/', (req,res) => {
     console.log('hello from frontend!')
 })
 
-const axios = require("axios");
-const options = {
-    method: 'POST',
-    url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
-    params: {to: 'he', 'api-version': '3.0', profanityAction: 'NoAction', textType: 'plain'},
-    headers: {
-      'content-type': 'application/json',
-      'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
-      'x-rapidapi-key': `${process.env.TRANS_API_KEY}`
-    },
-    data: [{Text: 'I would really like to drive your car around the block a few times.'}]
-  };
+
+
+
+app.post('/translate', (req,res) => {
+    const options = {
+        method: 'POST',
+        url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
+        params: {to: 'he', 'api-version': '3.0', profanityAction: 'NoAction', textType: 'plain'},
+        headers: {
+          'content-type': 'application/json',
+          'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
+          'x-rapidapi-key': `${process.env.TRANS_API_KEY}`
+        },
+        data: [{Text: 'I would really like to drive your car around the block a few times.'}]
+    };
+    axios.request(options).then(function (response) {
+        console.log(response.data[0].translations);
+    }).catch(function (error) {
+        console.error(error);
+    });
+})
   
-  axios.request(options).then(function (response) {
-      console.log(response.data[0].translations);
-  }).catch(function (error) {
-      console.error(error);
-  });
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
